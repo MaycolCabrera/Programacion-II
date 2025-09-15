6c0876a0aef1c9ccf242c9a5da970c459323949a
MaycolCabrera <mcabrerac10@miumg.edu.gt>
2025-09-14 18:22:53 -0600

    Add files via upload

---
+++ ClinicaVeterinaria/src/main/java/Mascota.java
@@ -0,0 +1,16 @@
+public class Mascota {
+    private String nombre;
+    private String especie; // perro, gato, etc.
+    private int edad;
+
+    public Mascota(String nombre, String especie, int edad) {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.edad = edad;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getEspecie() { return especie; }
+    public int getEdad() { return edad; }
+}
+
